6697aedccdeb7e3b4033a36d4dc65ee2becbc41e
roger1292 <roger1292@gmail.com>
2023-02-12 22:57:23 +0000

    complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -1,7 +1,7 @@
 
 public class Lab {
     /**
-     * Your task: you will recieve an array and a number, and you will have to set every element of the array to that
+     * Your task: you will receive an array and a number, and you will have to set every element of the array to that
      * number.
      *
      * You will need to access every element of the array and change them, one by one. For this type of
@@ -23,6 +23,8 @@ public class Lab {
      * @return nothing, because of pass-by-reference, all changes will be reflected in the original array.
      */
     public void set(int[] arr, int n){
-
+        for(int i = 0; i < arr.length; i++){
+            arr[i] = n;
+        }
     }
 }
